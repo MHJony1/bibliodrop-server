5660116b8fc9f2e87ad110ad97b71bc6c64f3bc4
jonym7293-alt <jonym7293@gmail.com>
2026-06-20 06:11:56 +0600

    create payments api for paymentsCollection

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+
 // const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 dotenv.config();
+
+const Stripe = require('stripe');
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const uri = process.env.MONGODB_URI;
 const port = process.env.PORT || 5000;
@@ -37,6 +41,7 @@ async function run() {
 
     const db = client.db('bibliodrop_db');
     const booksCollection = db.collection('books');
+    const paymentCollection = db.collection('payments');
 
     // books related api for all books with search, category, price, availability
     app.get('/api/books', async (req, res) => {
@@ -159,6 +164,66 @@ async function run() {
         });
       }
     });
+
+    // payment related api for stripe checkout
+    app.post('/api/payment-success', async (req, res) => {
+      try {
+        const { sessionId } = req.body;
+
+        if (!sessionId) {
+          return res.status(400).json({ error: 'Session ID is required' });
+        }
+        const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+        if (session.payment_status === 'paid') {
+          const existingPayment = await paymentCollection.findOne({
+            stripeSessionId: sessionId,
+          });
+
+          if (!existingPayment) {
+            const paymentRecord = {
+              userId: session.metadata.userId,
+              bookId: session.metadata.bookId,
+              bookTitle: session.metadata.bookTitle,
+              customerEmail: session.customer_details.email,
+              amountPaid: session.amount_total / 100,
+              stripeSessionId: sessionId,
+              paymentStatus: 'paid',
+              createdAt: new Date(),
+            };
+
+            const result = await paymentCollection.insertOne(paymentRecord);
+
+            if (session.metadata?.bookId) {
+              await booksCollection.updateOne(
+                { _id: new ObjectId(session.metadata.bookId) },
+                { $set: { status: 'Pending Delivery' } },
+              );
+            }
+            return res
+              .status(200)
+              .json({ success: true, message: 'Saved to MongoDB!', result });
+          }
+
+          return res
+            .status(200)
+            .json({ success: true, message: 'Already logged.' });
+        }
+
+        return res
+          .status(400)
+          .json({ error: 'Stripe transaction not completed' });
+      } catch (error) {
+        console.error('Server DB Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    });
+
+
+
+
+
+
 
 
 
